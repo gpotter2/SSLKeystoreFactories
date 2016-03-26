6ce0aa672074c8f87e9a84ed81dc7705fe888c54
gpotter2 <gpotter2@users.noreply.github.com>
2016-03-26 01:08:27 +0100

    Updated many options
    Updated keymanager system: may use several key managers if possible
    Added a different ssl context to use with an enum, upgraded info texts, added a configuration part at the beggining

--- src/fr/gpotter2/SSLKeystoreFactories/SSLServerSocketKeystoreFactory.java
+++ src/fr/gpotter2/SSLKeystoreFactories/SSLServerSocketKeystoreFactory.java
@@ -45,6 +45,52 @@ import javax.net.ssl.X509TrustManager;
  */
 public class SSLServerSocketKeystoreFactory {
 	
+	private static String instance;
+	/**
+	 * CONFIGURATION SECTION
+	 */
+	static {
+		instance = "JKS"/* TODO REPLACE WITH BKS IF USING IT*/;
+		/*
+		 * Several Notes: 
+		 * - Android only works with BKS, so you need to use only BKS certs files
+		 * - As before Android 15, BKS-v1 was used, you need to convert BKS in BKS-v1 to use it in Android 15-; BUT as Android 23+ doesn't support BKS-v1
+		 * and as BKS-v1 is deprecated, you need to have both of the certs and use them in fuction of the version
+		 * - Java doesn't support BKS without library
+		 * - A BKS format client can be connected a JKS format server
+		 */
+	}
+	
+	/**
+	 * 
+	 * A SSL algorithms types chooser enum
+	 * 
+	 * @author gpotter2
+	 *
+	 */
+	public static enum ServerSecureType {
+		@Deprecated
+		SSL("SSL"),
+		@Deprecated
+		SSLv2("SSLv2"),
+		SSLv3("SSLv3"),
+		@Deprecated
+		TLS("TLS"),
+		@Deprecated
+		TLSv1("TLSv1"),
+		TLSv1_1("TLSv1.1"),
+		TLSv1_2("TLSv1.2");
+		
+		private String type;
+		
+		private ServerSecureType(String type){
+			this.type = type;
+		}
+		public String getType(){
+			return type;
+		}
+	}
+	
 	/**
 	 * 
 	 * @param ip The IP to connect the socket to
@@ -60,14 +106,14 @@ public class SSLServerSocketKeystoreFactory {
 	 * @throws UnrecoverableKeyException Cannot get the keys of the KeyStore
 	 * @author gpotter2
 	 */
-	public static SSLServerSocket getServerSocketWithCert(int port, String pathToCert, String passwordFromCert) throws IOException,
+	public static SSLServerSocket getServerSocketWithCert(int port, String pathToCert, String passwordFromCert, ServerSecureType type) throws IOException,
 									KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, UnrecoverableKeyException{
 		File f = new File(pathToCert);
 		if(!f.exists()){
 			new NullPointerException("The specified path point to a non existing file !");
 			return null;
 		}
-		return getServerSocketWithCert(port, new FileInputStream(f), passwordFromCert);
+		return getServerSocketWithCert(port, new FileInputStream(f), passwordFromCert, type);
 	}
 	
 	/**
@@ -85,20 +131,20 @@ public class SSLServerSocketKeystoreFactory {
 	 * @throws UnrecoverableKeyException Cannot get the keys of the KeyStore
 	 * @author gpotter2
 	 */
-	public static SSLServerSocket getServerSocketWithCert(int port, InputStream pathToCert, String passwordFromCert) throws IOException,
+	public static SSLServerSocket getServerSocketWithCert(int port, InputStream pathToCert, String passwordFromCert, ServerSecureType type) throws IOException,
 									KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, UnrecoverableKeyException{
-		TrustManager[] tmm = new TrustManager[1];
-		KeyManager[] kmm = new KeyManager[1];
-		KeyStore ks  = KeyStore.getInstance("JKS");
+		X509TrustManager[] tmm;
+		X509KeyManager[] kmm;
+		KeyStore ks  = KeyStore.getInstance(instance);
 		ks.load(pathToCert, passwordFromCert.toCharArray());
-		tmm[0]=tm(ks);
-		kmm[0]=km(ks, passwordFromCert);
-		SSLContext ctx = SSLContext.getInstance("SSL");
+		tmm=tm(ks);
+		kmm=km(ks, passwordFromCert);
+		SSLContext ctx = SSLContext.getInstance(type.getType());
 		ctx.init(kmm, tmm, null);
 		SSLServerSocketFactory socketFactory = (SSLServerSocketFactory) ctx.getServerSocketFactory();
-		return (SSLServerSocket) socketFactory.createServerSocket(port);
+		SSLServerSocket ssocket = (SSLServerSocket) socketFactory.createServerSocket(port);
+		return ssocket;
 	}
-	
 	/**
 	 * Util class to get the X509TrustManager
 	 * 
@@ -109,13 +155,15 @@ public class SSLServerSocketKeystoreFactory {
 	 * @throws KeyStoreException
 	 * @author gpotter2
 	 */
-	private static X509TrustManager tm(KeyStore keystore) throws NoSuchAlgorithmException, KeyStoreException {
+	private static X509TrustManager[] tm(KeyStore keystore) throws NoSuchAlgorithmException, KeyStoreException {
 		TrustManagerFactory trustMgrFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         trustMgrFactory.init(keystore);
         TrustManager trustManagers[] = trustMgrFactory.getTrustManagers();
         for (int i = 0; i < trustManagers.length; i++) {
             if (trustManagers[i] instanceof X509TrustManager) {
-                return (X509TrustManager) trustManagers[i];
+            	X509TrustManager[] tr = new X509TrustManager[1];
+            	tr[0] = (X509TrustManager) trustManagers[i];
+                return tr;
             }
         }
         return null;
@@ -131,13 +179,15 @@ public class SSLServerSocketKeystoreFactory {
      * @throws UnrecoverableKeyException
      * @author gpotter2
      */
-    private static X509KeyManager km(KeyStore keystore, String password) throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+    private static X509KeyManager[] km(KeyStore keystore, String password) throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
 		KeyManagerFactory keyMgrFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
         keyMgrFactory.init(keystore, password.toCharArray());
         KeyManager keyManagers[] = keyMgrFactory.getKeyManagers();
         for (int i = 0; i < keyManagers.length; i++) {
             if (keyManagers[i] instanceof X509KeyManager) {
-                return (X509KeyManager) keyManagers[i];
+            	X509KeyManager[] kr = new X509KeyManager[1];
+            	kr[0] = (X509KeyManager) keyManagers[i];
+                return kr;
             }
         }
         return null;
